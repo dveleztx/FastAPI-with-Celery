7455624e75ddfc52c45a9e2be99c641a8a9c280d
David Velez <dveleztx@gmail.com>
2021-08-24 00:04:47 -0500

    Initial commit. Using celery for queueing a task. Right now emulating the 'processing' part with a time delay. Using flower as a dashboard for tracking celery tasks. Redis is used as the message broker and backend.

---
+++ main.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+###############################################################################
+# Script      : main.py
+# Author      : David Velez
+# Date        : 08/23/2021
+# Description : FastAPI and Celery Project
+###############################################################################
+
+# Imports
+from celery import Celery
+from fastapi import FastAPI
+
+app = FastAPI()
+
+celery = Celery(
+    __name__,
+    broker="redis://127.0.0.1:6379/0",
+    backend="redis://127.0.0.1:6379/0",
+)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello, World"}
+
+@celery.task
+def divide(x, y):
+    import time
+    time.sleep(5)
+    return x/y
